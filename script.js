3e8cb8677719e60ac6b8182a14f56ca8d86f64c0
Bukola Testimony <102736653+Bukola-Testimony@users.noreply.github.com>
2022-06-26 20:28:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ //Get access to the button
+        const  <li><a href="">Home</a></li> = document.querySelector('button');
+
+        //Add event listener to the button
+         <li><a href="">Home</a></li>.addEventListener('click', showMyName);
+
+        //Function to show my name
+        function showMyName() {
+            alert('Welcome!!\n My name is Bukola Testimony. How may I help you?');
+        }
+
